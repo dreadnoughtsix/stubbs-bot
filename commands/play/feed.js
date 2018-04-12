01c6f303ed6ffd17b0d6d121871e956bcf14fbcf
dreadnoughtsix <caba.james@yahoo.ca>
2018-04-11 21:54:59 -0400

    Add interval variable
    The interval variable will be changed when the user inputs a valid food
    item as Stubbs will have to sniff it first before eating it.

--- commands/play/feed.js
+++ commands/play/feed.js
@@ -1,5 +1,4 @@
 const {Command} = require('discord.js-commando');
-var testMsg = '(TESTING: play command group)';
 let foodCount = 0;
 
 module.exports = class FeedCommand extends Command {
@@ -8,7 +7,7 @@ module.exports = class FeedCommand extends Command {
             name: 'feed',
             group: 'play',
             memberName: 'feed',
-            description: 'Pet Stubbs',
+            description: 'Feed Stubbs',
             examples: ['feed <food>'],
             args: 
             [
@@ -24,19 +23,23 @@ module.exports = class FeedCommand extends Command {
 
     run(message, {food}) {
         let returnMsg = '';
-        console.log('Food Count:', foodCount);
+        let interval = 1000;
+
         if (foodCount == 1) {
             returnMsg += 'I need to poop now **-runs to the toilet-**';
-        }
-        else if (!food) {
+
+        } else if (!food) {
             returnMsg += 'Oh, you don\'t have any food for me? **-cries-**';
+
         } else {
             message.say('**-sniffs food-**');
             foodCount++;
+            interval = 2000;
             returnMsg += 'I love ' + food + '! **-munch munch munch-**';
         }
+
         setTimeout(function() {
             message.say(returnMsg);
-        }, 2000);
+        }, interval);
     }
 };
